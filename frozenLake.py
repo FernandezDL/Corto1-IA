b19f26078a11f5d44b61a3400bb37b8b07de62a8
FernandezDL <dianaferbandez31@gmail.com>
2024-03-01 15:45:21 -0600

    Add: testing
    Codigo para probar el modelo entrenado

--- frozenLake.py
+++ frozenLake.py
@@ -9,7 +9,7 @@ action_space_size = env.action_space.n
 state_space_size = env.observation_space.n
 q_table = np.random.rand(state_space_size, action_space_size)
 
-num_episodes = 100
+num_episodes = 300
 max_steps_per_episode = 100
 
 learning_rate = 0.1
@@ -23,7 +23,6 @@ exploration_decay_rate = 0.001
 rewards_all_episodes = []
 
 for episode in range(num_episodes):
-    print(episode)
     state = env.reset()[0]
     done = False
     rewards_current_episode = 0
@@ -44,10 +43,33 @@ for episode in range(num_episodes):
         rewards_current_episode += reward
 
         if done:
+            print(f"Episode {episode + 1} finished after {step + 1} steps with {'Success' if reward == 1 else 'Failure'}")
+
             break
 
     exploration_rate = min_exploration_rate + \
                         (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate * episode)
     rewards_all_episodes.append(rewards_current_episode)
 
+num_test_episodes = 100
+total_test_reward = 0
+
+for episode in range(num_test_episodes):
+    state = env.reset()[0]
+    done = False
+    episode_reward = 0
+
+    while not done:
+        action = np.argmax(q_table[state, :])
+        new_state, reward, done, _, _ = env.step(action)
+        state = new_state
+        episode_reward += reward
+
+    total_test_reward += episode_reward
+    print(f"Test Episode {episode + 1}: Reward = {episode_reward}")
+
+print("=========================================================================")
+print("Promedio de exitos: ", total_test_reward/100)
+
 env.close()
+
